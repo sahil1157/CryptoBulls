c3ba0720a8941d235b46fecfb3a16a6443932aa2
dhiren <dhiren.parmar@bombaysofware.com>
2024-06-08 00:19:47 +0530

    Scroll behaiour and active issue changes.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from 'react'
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
-import EndFooter from './Components/Footer/EndFooter';
+import React, { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import EndFooter from "./Components/Footer/EndFooter";
 
 const App = () => {
-  return (
-    <div className='flex flex-col min-h-screen'>
-      {/* <Routing /> */}
-      <Navbar/>
-      <Home/>
-      <EndFooter/>
-    </div>
-  )
-}
+    const [activeSection, setActiveSection] = useState('strategy');
+    const [showScrollTop, setShowScrollTop] = useState(false);
+	const handleScroll = () => {
+		const sections = document.querySelectorAll("section");
+		let currentSection = "strategy";
+		sections.forEach((section) => {
+			const sectionTop = section.offsetTop;
+			if (window.scrollY >= sectionTop - 60) {
+				currentSection = section.getAttribute("id");
+			}
+		});
+		setActiveSection(currentSection);
 
-export default App
+		if (window.scrollY > 300) {
+			setShowScrollTop(true);
+		} else {
+			setShowScrollTop(false);
+		}
+	};
+
+	const scrollToSection = (id) => {
+		const section = document.getElementById(id);
+		section.scrollIntoView({ behavior: "smooth" });
+	};
+
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+	return (
+		<div className="flex flex-col min-h-screen">
+			{/* <Routing /> */}
+			<Navbar activeSection={activeSection} scrollToSection={scrollToSection} showScrollTop={showScrollTop} />
+			<Home />
+			<EndFooter />
+            {showScrollTop && (
+                <button className="scroll-to-top" onClick={scrollToTop}>↑</button>
+            )}
+		</div>
+	);
+};
+
+export default App;
